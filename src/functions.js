e17b988ff6bb98f4e59122219758dea7edc1bea5
Jared Farkas and Stuart McKay <jared+stuart@>
2019-06-04 16:55:52 -0700

    make skillpoints and infection rate increase turn after turn

--- src/functions.js
+++ src/functions.js
@@ -1,11 +1,11 @@
 export class Game{
     constructor(player){
         this.player = player;
-        this.population = {N: { Healthy : 100, Infected : 1, Dead : 0, Immune: 0},
-                           NE: {Healthy : 100, Infected : 1, Dead : 0, Immune: 0},
-                           NW: {Healthy : 100, Infected : 1, Dead : 0, Immune: 0},
-                           SE: {Healthy : 100, Infected : 1, Dead : 0, Immune: 0},
-                           SW: {Healthy : 100, Infected : 1, Dead : 0, Immune: 0}, }
+        this.population = {N: { Healthy : 100, Infected : 0, Dead : 0, Immune: 0},
+                           NE: {Healthy : 100, Infected : 0, Dead : 0, Immune: 0},
+                           NW: {Healthy : 100, Infected : 0, Dead : 0, Immune: 0},
+                           SE: {Healthy : 100, Infected : 0, Dead : 0, Immune: 0},
+                           SW: {Healthy : 100, Infected : 0, Dead : 0, Immune: 0}, }
         this.skillpoints = 0;
         this.skills = {};
         this.infectionRate = .1;
--- test/main.test.js
+++ test/main.test.js
@@ -6,9 +6,21 @@ describe('Game turn tick functions', function () {
         game.tick();
         expect(game.skillpoints).toEqual(1);    
     });
+    test('Add skillpoint each tick cummulative', () => {
+        let game = new Game("Jared");
+        game.tick();
+        game.tick();
+        expect(game.skillpoints).toEqual(2);    
+    });
     test('Infection rate to increase', () => {
         let game = new Game("Jared");
         game.tick();
         expect(game.infectionRate).toEqual(.11);    
+    });
+    test('Infection rate increase turn after turn', () => {
+        let game = new Game("Jared");
+        game.tick();
+        game.tick();
+        expect(game.infectionRate).toEqual(.121);    
     });
 });
